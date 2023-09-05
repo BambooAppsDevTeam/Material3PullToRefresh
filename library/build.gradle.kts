8e9ba0f20893419273c5ca9ebcdd116280d78d96
Andrei Mukamolau <amv@bamboogroup.eu>
2023-09-05 10:16:59 +0300

    Cleaned up version catalog

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     kotlinOptions {
         jvmTarget = "1.8"
