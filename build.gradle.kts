4e0f9f89c090a2e2719b9b3c8dfcbcd398a52e92
Andrei Mukamolau <amv@bamboogroup.eu>
2023-09-05 13:04:26 +0300

    Added publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,14 @@ plugins {
     alias(libs.plugins.android.library) apply false
     kotlin("android") version libs.versions.kotlin apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.publishing)
+}
+
+nexusPublishing {
+    this.repositories {
+       sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
 }
