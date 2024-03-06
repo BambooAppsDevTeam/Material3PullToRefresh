96a05161e9fa1c942e4732127a5ca62f2449f02c
Andrei Mukamolau <amv@bamboogroup.eu>
2024-03-06 15:22:28 +0200

    Updated dependencies

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -76,7 +76,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "eu.bambooapps"
             artifactId = "compose-material3-pullrefresh"
-            version = "1.1.0"
+            version = "1.1.1"
 
             artifact(javadocJar)
 
